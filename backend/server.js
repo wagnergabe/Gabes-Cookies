108b3ec4c0ee4abe985b9ec53e551aceb6c67873
unknown <gabrielswagner7@gmail.com>
2024-09-17 19:26:10 -0500

    backend update

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 import connectDB from './config/db.js';
 import cors from 'cors';
 import cookieRoutes from './routes/cookieRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 dotenv.config();
 
@@ -11,12 +13,19 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express()
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
 app.use(cors({ origin: 'http://localhost:3000' }));
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
 app.use('/api/cookies', cookieRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
